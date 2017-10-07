8a1a3c15d5287edeb6c0d532fcd1315c0fb9f1ea
Sidath <sidath.14@cse.mrt.ac.lk>
2017-10-07 13:08:00 +0530

    added home and navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import  { NavbarComponent } from './shared/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
